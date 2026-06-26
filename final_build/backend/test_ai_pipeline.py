4dea9e87ca5b69c29e9a51d3ba1b47c4e76f9725
Mubeen <141475267+muhmubeen1@users.noreply.github.com>
2026-06-26 19:36:50 +0500

    Clean project files for deployment

--- final_build/backend/test_ai_pipeline.py
+++
@@ -1,7 +0,0 @@
-from ai_services.ai_pipeline import run_ai_pipeline
-
-video_path = "uploads/test.mp4"
-
-result = run_ai_pipeline(video_path)
-
-print(result)
--- final_build/backend/test_analyze_video.py
+++
@@ -1,7 +0,0 @@
-from ai_services.ai_pipeline import analyze_video
-
-video_path = "uploads/test.mp4"
-
-result = analyze_video(video_path)
-
-print(result)
